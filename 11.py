fcdfde094e25aa598ef51f28190e2dcc03b95aff
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-09-17 20:54:22 +0800

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,12 +1,8 @@
 list = input()
 list1 = []
 for i in range(0,len(list)):
-    list1.append(list[i])
+    if list[i] != " ":
+        list1.append(list[i])
 for i in range(0,len(list1)):
-    print(list1[len(list) - i - 1],end = "")
-print(' ')
-
-i =  len(list1) - 1
-while i >= 0:
     print(list1[i],end = "")
-    i = i - 1
+print(" ")
