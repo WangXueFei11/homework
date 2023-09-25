41819978b3d6006863d3527e9b47ff78abf6b94a
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-09-25 14:01:07 +0800

    Update 07.py

--- 07.py
+++ 07.py
@@ -1,3 +1,7 @@
-for i in range(0,100):
-    if i % 2 == 1:
-        print(i,end = " ")
+c = int(input())
+g = c / 2
+i = 0
+while abs(g * g * g - c) > 0.00000000001:
+    g = (2 * g + c / (g * g)) / 3
+    i += 1
+    print("%d: %.15f" %(i,g))
