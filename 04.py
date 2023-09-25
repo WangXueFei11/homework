ad5457df638fdb3156751164bd5f01d71b0d9f7f
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-09-25 13:55:37 +0800

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,5 +1,14 @@
-print(chr(0x2a) * 20)
-print(chr(0x2a),end = '')
-print('数据科学与工程导论',end = "")
-print(chr(0x2a))
-print(chr(0x2a) * 20)
+def Square_root_1():
+    c = 2
+    i = 0
+    g = 0
+    for j in range(0,c + 1):
+        while (j * j) < c:
+            j += 1
+        g = j - 1
+    while abs(g * g - c) > 0.0001:
+        g += 0.00001
+        i += 1
+        print("%d:g = %.5f" %(i,g))
+        
+Square_root_1()
