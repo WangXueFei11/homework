e0ac3e622f12636c3aa7f2718111c39520d8ecfd
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-09-25 13:59:38 +0800

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,12 +1,31 @@
-w = int(input())
-x = int(input())
-y = int(input())
-z = int(input())
-list = []
-list.append(w)
-list.append(x)
-list.append(y)
-list.append(z)
-list.sort(reverse = True)
-for i in list:
-    print(i,end = " ")
+def square_root_2():
+    c = 2
+    g = c / 2
+    i = 0
+    while abs((g * g) - c) > 0.00000000001:
+        g = (g + c / g) / 2
+        i += 1
+        print("%d:%.13f"%(i,g))
+        
+def square_root_3():
+    c = 2
+    g = c / 4
+    i = 0
+    while abs((g * g) - c) > 0.00000000001:
+        g = (g + c / g) / 2
+        i += 1
+        print("%d:%.13f"%(i,g))
+
+def square_root_4():
+    c = 2
+    g = c
+    i = 0
+    while abs((g * g) - c) > 0.00000000001:
+        g = (g + c / g) / 2
+        i += 1
+        print("%d:%.13f"%(i,g))
+square_root_2()
+print("\n",end = "")
+square_root_3()
+print("\n",end = "")
+square_root_4()
