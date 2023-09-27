172b3f8ce6ad60618d195485f0bbf32428a9d339
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-09-27 08:32:02 +0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,5 +1,14 @@
 def max_multi(n):
-    if n % 3 == 0:
+    if n == 1:
+        print('0')
+        return
+    elif n == 2:
+        print('1')
+        return
+    elif n == 3:
+        print('2')
+        return
+    elif n % 3 == 0:
         result = 3 ** (n / 3)
     elif n % 3 == 1:
         result = 4 * 3 ** ((n - 4) / 3)
