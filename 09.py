5cccc53a3d3df93e2ab9ee5aa01d645a317a5e7c
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-10-09 15:52:33 +0800

    Update 09.py

--- 09.py
+++ 09.py
@@ -1,12 +1,8 @@
-import random
-import math
-def estimate(times):
-    sum = 0
-    for i in range(times):
-        x = random.uniform(2,3)
-        y = random.uniform(0,100)
-        d = x * x + 4 * x * math.sin(x) - y
-        if d > 0:
-            sum += 1
-    print((sum * 100) / times)
-estimate(1000000)
+B = []
+list1 = input()
+list1 = list1.split(' ')
+sum = 1
+for i in list1:
+       sum *= int(i)
+       B.append(sum)
+print(B)
