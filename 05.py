17123f28cb596b6ed066f4140c4a6e75fc193c1d
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-10-09 15:36:31 +0800

    Update 05.py

--- 05.py
+++ 05.py
@@ -1,10 +1,3 @@
-def square_root_3(x):
-    c = x 
-    g = c / 2
-    i = 0
-    while abs((g * g) - c) > 0.00000000001:
-        g = (g + c / g) / 2
-        i += 1
-        print("%d:%.13f"%(i,g))
-x = int(input())
-square_root_3(x)
+for i in range(1,101):
+    if i % 2 == 0:
+        print(i,end = " ")
