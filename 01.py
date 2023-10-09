333321bb0bea06f0a49965446e64952397b90fec
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-10-09 15:31:25 +0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,20 +1,13 @@
-def max_multi(n):
-    if n == 1:
-        print('0')
-        return
-    elif n == 2:
-        print('1')
-        return
-    elif n == 3:
-        print('2')
-        return
-    elif n % 3 == 0:
-        result = 3 ** (n / 3)
-    elif n % 3 == 1:
-        result = 4 * 3 ** ((n - 4) / 3)
-    elif n % 3 == 2:
-        result = 2 * 3 ** ((n - 2) / 3)
-    print(result)
-
-n = int(input())
-max_multi(n)
+x = float(input())
+list1 = []
+i = 0
+while x != 0:
+    if x - 0.5 ** (i + 1) >= 0:
+        x = x - 0.5 ** (i + 1)
+        list1.append(1)
+    else:
+        list1.append(0)
+    i += 1
+print('0.',end = '')
+for i in range(len(list1)):
+    print(list1[i],end = '')
