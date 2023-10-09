1a3c6ccd48a13b5c7f1e051f731b003b09f72b00
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-10-09 15:37:46 +0800

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,31 +1,10 @@
-def square_root_2():
-    c = 2
-    g = c / 2
-    i = 0
-    while abs((g * g) - c) > 0.00000000001:
-        g = (g + c / g) / 2
-        i += 1
-        print("%d:%.13f"%(i,g))
-        
-def square_root_3():
-    c = 2
-    g = c / 4
-    i = 0
-    while abs((g * g) - c) > 0.00000000001:
-        g = (g + c / g) / 2
-        i += 1
-        print("%d:%.13f"%(i,g))
-
-def square_root_4():
-    c = 2
-    g = c
-    i = 0
-    while abs((g * g) - c) > 0.00000000001:
-        g = (g + c / g) / 2
-        i += 1
-        print("%d:%.13f"%(i,g))
-square_root_2()
-print("\n",end = "")
-square_root_3()
-print("\n",end = "")
-square_root_4()
+# -*- coding: utf-8 -*-
+score = int(input())
+if score >= 90:
+    print("优秀")
+elif score < 90 and score >= 75:
+    print("良好")
+elif score < 75 and score >= 60:
+    print("合格")
+elif score < 60:
+    print("不合格")
