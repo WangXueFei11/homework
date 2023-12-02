9ccc9017be6d7b095db872bd7c6a371cc2e87e3e
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-12-02 22:54:53 +0800

    Delete 11.py

--- 11.py
+++
@@ -1,8 +0,0 @@
-list = input()
-list1 = []
-for i in range(0,len(list)):
-    if list[i] != " ":
-        list1.append(list[i])
-for i in range(0,len(list1)):
-    print(list1[i],end = "")
-print(" ")
