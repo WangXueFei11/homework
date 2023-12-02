a9699be5c5acdf83a62cb4254a97322267b8af49
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-12-02 23:28:05 +0800

    Create 11.6.py

---
+++ 11.6.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+dt = load_iris()
+X_train,X_test,y_train,y_test=train_test_split(dt['data'],dt['target'],test_size=0.3, random_state=42)
