7d97a7ca33c16d2e9f3152adb71acb830451dba6
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-12-02 22:54:39 +0800

    Delete 09.py

--- 09.py
+++
@@ -1,9 +0,0 @@
-list = [1,2,3,4,5]
-for i in range(0,len(list)):
-    print(list[len(list) - i - 1],end = " ")
-print(' ')
-
-i =  len(list) - 1
-while i >= 0:
-    print(list[i],end = " ")
-    i = i - 1
