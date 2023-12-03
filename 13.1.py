fc0e6cac38c58d3809184200d1610d6b99d5ab66
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-12-03 17:19:48 +0800

    Update 13.1.py

--- 13.1.py
+++ 13.1.py
@@ -1,17 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-from sklearn.datasets import load_iris
-
-data = load_iris()
-X = data.data
-y = data.target
-feature_names = data.feature_names
-
-df = pd.DataFrame(data=np.c_[X, y], columns=feature_names + ['target'])
-df['species'] = df['target'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
-
-sns.set(style="ticks")
-sns.pairplot(df, hue='species', markers=["o", "s", "D"])
+# -*- coding: utf-8 -*-
+from sklearn import datasets  
+import matplotlib.pyplot as plt  
+  
+# 导入iris数据集  
+iris = datasets.load_iris()  
+  
+# 获取特征数据和标签数据  
+X = iris.data  
+y = iris.target  
+  
+# 可视化花的长度（单位：厘米）与花的宽度（单位：厘米）的关系  
+plt.figure(figsize=(10, 6))  
+plt.scatter(X[:, 2], X[:, 3])  # 第0列为花的类别，第2列和第3列分别是花的长度和宽度  
+plt.xlabel('length')  
+plt.ylabel('width')  
+plt.title('relationship')  
+plt.show()  
+  
+# 可视化花的形状（单位：厘米）与花的颜色（单位：厘米）的关系  
+plt.figure(figsize=(10, 6))  
+plt.scatter(X[:, 1], X[:, 0])  # 第1列和第0列分别是花的形状和颜色  
+plt.xlabel('length')  
+plt.ylabel('color')  
+plt.title('relationship')  
 plt.show()
