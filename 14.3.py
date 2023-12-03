a9d18d6fde5d0c24fd0a24bd9e2ddef938939921
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2023-12-03 17:51:07 +0800

    Update 14.3.py

--- 14.3.py
+++ 14.3.py
@@ -1,10 +1,15 @@
-from sklearn.feature_extraction.text import  TfidfVectorizer
+# -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_20newsgroups
-
-dt=fetch_20newsgroups()
-text=[dt.data[0]]
-tv=TfidfVectorizer()
-v=tv.fit_transform(text)
-print(v)
-print(tv.get_feature_names_out())
-print(v.toarray())
+from sklearn.feature_extraction.text import TfidfVectorizer
+# 加载数据集
+newsgroups_train = fetch_20newsgroups(subset='train')
+# 创建TfidfVectorizer对象
+vectorizer = TfidfVectorizer()
+# 对文本进行向量化 
+vectors = vectorizer.fit_transform(newsgroups_train.data)
+# 获取特征名（即词汇表）
+feature_names = vectorizer.get_feature_names_out()
+# 输出第一个文本的结果向量
+first_document_vector = vectors[0]
+print("first_document_vector:")
+print(first_document_vector)
